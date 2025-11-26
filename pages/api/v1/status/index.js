26ec0ca547c66ad1ff25b63fa28aed33889f96b5
DonCarderms <103117597+DonCarderms@users.noreply.github.com>
2025-11-26 11:54:43 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
 //  /api/v1/satus
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
   response
     .status(200)
     .json({ affirmacao: "sou muito foda e em cima da média" });
