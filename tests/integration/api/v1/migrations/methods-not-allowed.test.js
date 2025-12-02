d6f699c5edda5ea594312146c9583257d25577e3
DonCarderms <103117597+DonCarderms@users.noreply.github.com>
2025-12-02 16:21:08 -0300

    test: rename all descriptions to use new patterns

--- tests/integration/api/v1/migrations/methods-not-allowed.test.js
+++ tests/integration/api/v1/migrations/methods-not-allowed.test.js
@@ -3,20 +3,27 @@ import orchestrator from "tests/orchestrator.js";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
+describe("Method Not Allowed to api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Making a DELETE request", async () => {
+      const responseDelete = await fetch(
+        "http://localhost:3000/api/v1/migrations",
+        {
+          method: "DELETE",
+        },
+      );
 
-test("Method Not Allowed to api/v1/migrations should close the server", async () => {
-  const responseDelete = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "DELETE",
-    },
-  );
+      expect(responseDelete.status).toBe(405);
+    });
+    test("Making a PUT request", async () => {
+      const responsePut = await fetch(
+        "http://localhost:3000/api/v1/migrations",
+        {
+          method: "PUT",
+        },
+      );
 
-  expect(responseDelete.status).toBe(405);
-
-  const responsePut = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PUT",
+      expect(responsePut.status).toBe(405);
+    });
   });
-
-  expect(responsePut.status).toBe(405);
 });
