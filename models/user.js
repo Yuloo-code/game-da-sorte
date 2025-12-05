8bdd95f3b662ee1fb0b4ad3b6b06c7e3e1e679c6
DonCarderms <103117597+DonCarderms@users.noreply.github.com>
2025-12-05 10:58:44 -0300

    feat: create `user.FindOneByUsername` method and `/api/v1/users/[usernme]` enpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { ValidationError, NotFoundError } from "infra/errors";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -68,8 +68,37 @@ async function create(userInputValues) {
   }
 }
 
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT
+            *
+        FROM
+            users
+        WHERE
+            LOWER(username) = LOWER($1)
+        LIMIT
+            1
+     ;`,
+      values: [username],
+    });
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+    return results.rows[0];
+  }
+  return userFound;
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
