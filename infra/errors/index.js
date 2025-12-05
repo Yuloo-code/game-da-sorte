453d55d1d4a5d2bb633bfb35cbcb6920f3720626
DonCarderms <103117597+DonCarderms@users.noreply.github.com>
2025-12-05 10:53:18 -0300

    feat: add `NotFoundErrror` in `controller.js` and `errors`

--- infra/errors/index.js
+++ infra/errors/index.js
@@ -75,3 +75,22 @@ export class ValidationError extends Error {
     };
   }
 }
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "não foi possível encontrar este recurso no sistema", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action = action || "Verifique os parâmetros enviados na consulta";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
