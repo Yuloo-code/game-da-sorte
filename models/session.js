8d1210eaa75c8f6606bb4b771cf82b440d8cff61
DonCarderms <103117597+DonCarderms@users.noreply.github.com>
2025-12-07 08:14:45 -0300

    fix: fix import `NotFoundError`

--- models/session.js
+++ models/session.js
@@ -1,5 +1,6 @@
 import crypto from "node:crypto";
 import database from "infra/database.js";
+import { NotFoundError } from "infra/errors/index.js";
 
 const EXPIRATIO_IN_MILISECONDS = 60 * 60 * 24 * 30 * 1000; // 30 days
 
