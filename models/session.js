314e306e6d7620c7d5d520f93d9567e7f7b6e6f3
DonCarderms <103117597+DonCarderms@users.noreply.github.com>
2025-12-07 07:52:42 -0300

    feat: add `findOneValidByToken` and `findOneById` methods to `model` `session`

--- models/session.js
+++ models/session.js
@@ -26,8 +26,61 @@ async function create(userId) {
   }
 }
 
+async function findOneValidByToken(ProvidedSessionToken) {
+  const sessionFound = await runInsertQuery(ProvidedSessionToken);
+  return sessionFound;
+
+  async function runInsertQuery(sessionToken) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          sessions
+        WHERE
+          token = $1
+          AND expires_at > NOW()
+        LIMIT
+          1
+      `,
+      values: [sessionToken],
+    });
+    return results.rows[0];
+  }
+}
+
+async function findOneById(id) {
+  const userFound = await runSelectQuery(id);
+
+  async function runSelectQuery(id) {
+    const results = await database.query({
+      text: `
+        SELECT
+            *
+        FROM
+            users
+        WHERE
+            id = $1
+        LIMIT
+            1
+     ;`,
+      values: [id],
+    });
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O id informado não foi encontrado no sistema",
+        action: "Verifique se o id está digitado corretamente.",
+      });
+    }
+    return results.rows[0];
+  }
+  return userFound;
+}
+
 const session = {
   create,
+  findOneValidByToken,
+  findOneById,
   EXPIRATIO_IN_MILISECONDS,
 };
 
